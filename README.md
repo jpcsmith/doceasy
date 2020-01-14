da677c85dc0bb1b95b88c3b9796f406e45d6f975
Jean-Pierre Smith <rougesprit@gmail.com>
2020-01-14 14:41:43 +0100

    Add setup.py and readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# doceasy
+
+doceasy is (currently) a simple wrapper around docopt and schema to make working with them in scientific papers a bit less cumbersome.
---
+++ setup.py
@@ -0,0 +1,45 @@
+"""Installation module for doceasy.
+"""
+from os import path
+from pathlib import Path
+from setuptools import setup, find_packages
+
+
+setup(
+    name='doceasy',  # Required
+    description='Wrapper around docopt and schema',  # Optional
+    url='https://github.com/jpcsmith/doceasy',  # Optional
+    python_requires='>=3.5',
+
+    # Optional long description in README.md
+    long_description=Path(
+        path.join(path.abspath(path.dirname(__file__)), 'README.md')
+    ).read_text(),
+
+    # Automatically extract version information from git tags
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+
+    # This should be your name or the name of the organization which owns the
+    # project.
+    author='Jean-Pierre Smith',  # Optional
+
+    # This should be a valid email address corresponding to the author listed
+    # above.
+    author_email='rougesprit@gmail.com',  # Optional
+
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # Required
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+
+    install_requires=['docopt', 'schema'],  # Optional
+
+    extras_require={  # Optional
+        'dev': [],
+        'test': [],
+    },
+)
