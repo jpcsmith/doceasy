6f5db1fe4861202ae0402404a5b980eebc54ca84
Jean-Pierre Smith <rougesprit@gmail.com>
2020-01-14 15:20:23 +0100

    Correctly add module

--- setup.py
+++ setup.py
@@ -7,39 +7,25 @@ from setuptools import setup, find_packages
 
 setup(
     name='doceasy',  # Required
-    description='Wrapper around docopt and schema',  # Optional
-    url='https://github.com/jpcsmith/doceasy',  # Optional
-    python_requires='>=3.5',
-
-    # Optional long description in README.md
+    url='https://github.com/jpcsmith/doceasy',
+    author='Jean-Pierre Smith',
+    author_email='rougesprit@gmail.com',
+    description='Wrapper around docopt and schema',
     long_description=Path(
         path.join(path.abspath(path.dirname(__file__)), 'README.md')
     ).read_text(),
-
-    # Automatically extract version information from git tags
-    use_scm_version=True,
-    setup_requires=['setuptools_scm', 'wheel'],
-
-    # This should be your name or the name of the organization which owns the
-    # project.
-    author='Jean-Pierre Smith',  # Optional
-
-    # This should be a valid email address corresponding to the author listed
-    # above.
-    author_email='rougesprit@gmail.com',  # Optional
-
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # Required
-
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-
-    install_requires=['docopt', 'schema'],  # Optional
-
-    extras_require={  # Optional
+    py_modules=['doceasy'],
+    python_requires='>=3.5',
+    use_scm_version=True,
+    setup_requires=['setuptools_scm', 'wheel'],
+    install_requires=['docopt', 'schema'],
+    extras_require={
         'dev': [],
         'test': [],
-    },
+    }
 )
